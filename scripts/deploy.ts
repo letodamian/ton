199ec362aa7655f341b5b8066df05e4ba1eecd9d
yakkujit <yakkujit@gmail.com>
2024-05-13 23:35:40 +0400

    added blueprrint, added deposit and withddraw

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,47 +1,25 @@
-import { Cell, StateInit, beginCell, contractAddress, storeStateInit, toNano } from "ton-core";
-import { hex } from '../build/main.compiled.json';
-import QueryString from "qs";
-import { config } from "../config";
+import { NetworkProvider, compile } from "@ton-community/blueprint";
+import { MainContract } from "../wrappers/MainContract";
+import { address, toNano } from "ton-core";
 
-async function deployScript() {
-  const codeCell = Cell.fromBoc(Buffer.from(hex, "hex"))[0];
-  const dataCell = new Cell();
+export async function run(provider: NetworkProvider) {
+  const codeCell = await compile("MainContract");
 
-  const stateInit: StateInit = {
-    code: codeCell,
-    data: dataCell,
-  };
+  const myContract = MainContract.createFromConfig
+  (
+    {
+      number: 0,
+      address: address("UQD0QxaCiGjPU99zc9ZbWJGvq_aAoTHJ748K38wVNyJwWvjf"),
+      owner_address: address('UQD0QxaCiGjPU99zc9ZbWJGvq_aAoTHJ748K38wVNyJwWvjf')
+    },
+    codeCell,
+  );
 
-  const stateInitBuilder = beginCell();
-  storeStateInit(stateInit)(stateInitBuilder);
-  const stateInitCell = stateInitBuilder.endCell();
+  const openedContract = await provider.open(myContract);
+  openedContract.sendDeploy(
+    provider.sender(),
+    toNano("0.01")
+  );
 
-  const address = contractAddress(0, {
-    code: codeCell,
-    data: dataCell,
-  });
-
-  const deployLink =
-  'https://app.tonkeeper.com/transfer/' +
-  address.toString({
-      testOnly: config.isTestOnly,
-  }) +
-  "?" +
-  QueryString.stringify({
-      text: "Deploy contract by QR",
-      amount: toNano("0.0001").toString(10),
-      init: stateInitCell.toBoc({idx: false}).toString("base64"),
-  });
-
-  console.log(deployLink);
-
-  const  scanAddr = 
-    config.tonscanUrl +
-    address.toString({
-        testOnly: config.isTestOnly,
-    })
-
-  console.log(scanAddr);
+  await provider.waitForDeploy(myContract.address);
 }
-
-deployScript();
